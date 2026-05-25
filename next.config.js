4a673bc49a9cf862ca5a83cb16f2f3cdb00112bc
Navjot kaur <navjot.kaur@kilowott.com>
2026-05-25 15:19:47 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_IPIFY_API_KEY: "at_6L6NdauxsZ2DOZhAoqSEP4i9Uar19",
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   images: {
-    domains: ['www.themoviedb.org'],
+    domains: ["www.themoviedb.org"],
   },
   publicRuntimeConfig: {
-    IPIFY_API_KEY: process.env.NEXT_PUBLIC_IPIFY_API_KEY || 'at_6L6NdauxsZ2DOZhAoqSEP4i9Uar19',
+    IPIFY_API_KEY:
+      process.env.NEXT_PUBLIC_IPIFY_API_KEY ||
+      "at_6L6NdauxsZ2DOZhAoqSEP4i9Uar19",
   },
-  experimental: { 
-    images: { allowFutureImage: true }
-  }
-}
+  experimental: {
+    images: { allowFutureImage: true },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
